edfa5321f1b6895e1228a8dc1b7c76bbe95c605a
CRISTIAN OLARTE CARRASQUILLA <cristianolarte321414@correo.itm.edu.co>
2023-03-17 19:06:04 -0500

    Add project files.

---
+++ QuizArrays/Quiz.Arrays.Logic/TwoDimensions.cs
@@ -0,0 +1,58 @@
+﻿namespace Quiz.Arrays.Logic
+{
+    public class TwoDimensions
+    {
+        private int[,] _array;
+
+        public int N { get; }
+        public int M { get; }
+
+        public TwoDimensions(int _n, int _m)
+        {
+            N = _n; M = _m;
+            _array = new int[_n, _m];
+        }
+        public void Fill()
+        {
+            Fill(1, 100);
+        }
+        public void Fill(int min, int max)
+        {
+            var random = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    _array[i, j] = random.Next(min, max);
+                }
+            }
+        }
+        public override string ToString()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    output += $"{_array[i, j]}\t";
+                }
+                output += "\n";
+            }
+            return output;
+        }
+
+        public String ToOneDimension()
+        {
+            var output = string.Empty;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    output += $"{_array[i, j]}\t";
+                }                
+            }
+            return output;
+        }
+    
+    }
+}
---
+++ QuizArrays/QuizArrays.UI/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Quiz.Arrays.Logic;
+
+try
+{
+	Console.WriteLine("ARRAY QUIZ");
+	var twoDimensions = new TwoDimensions(5, 3);
+	twoDimensions.Fill();
+	Console.WriteLine("Two dimensions array =>");
+	Console.WriteLine(twoDimensions);
+	var oneDimension = twoDimensions.ToOneDimension();
+	Console.WriteLine("One dimension array =>");
+	Console.WriteLine(oneDimension);
+
+}
+catch (Exception ex)
+{
+
+    Console.WriteLine(ex.Message);
+}
